2f59503c295ce231ac25e00ca94940c9fd85b071
Daniel Richter <danielrichter@posteo.de>
2023-09-10 18:32:51 +0200

    change name for capi cluster
    Signed-off-by: Daniel Richter <danielrichter@posteo.de>

--- tools/createmanagementcluster.sh
+++ tools/createmanagementcluster.sh
@@ -1,13 +1,13 @@
 export EXP_CLUSTER_RESOURCE_SET=true
 
-kind create cluster --name clusterapi --kubeconfig ~/.kube/clusterapi
+kind create cluster --name capi-kind --kubeconfig ~/.kube/capi-kind
 
-KUBECONFIG=~/.kube/clusterapi clusterctl init --infrastructure hetzner --addon helm
+KUBECONFIG=~/.kube/capi-kind clusterctl init --infrastructure hetzner --addon helm
 
-KUBECONFIG=~/.kube/clusterapi velero install \
+KUBECONFIG=~/.kube/capi-kind velero install \
     --provider aws \
     --plugins velero/velero-plugin-for-aws:v1.2.1 \
-    --bucket clusterapi \
+    --bucket danielrichtercodes-capi-kind \
     --secret-file ./tools/backblaze.ini \
     --use-volume-snapshots=false \
     --backup-location-config region=s3.us-west-004,s3Url=https://s3.us-west-004.backblazeb2.com
